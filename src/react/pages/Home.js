428070059255f85af4e3573468c23c2ae4e499a5
Rob Spears <rob.sp3ars@gmail.com>
2019-11-19 12:07:31 -0500

    Modify Home page to support new messages and message feed components

--- src/react/pages/Home.js
+++ src/react/pages/Home.js
@@ -1,17 +1,16 @@
 import React from "react";
-import { LoginForm, Menu } from "../components";
-import { userIsNotAuthenticated } from "../HOCs";
+import { Menu } from "../components";
+import { userIsAuthenticated } from "../HOCs";
 
 class Home extends React.Component {
   render() {
     return (
       <>
-        <Menu />
-        <h2>Your favorite microblogging platform</h2>
-        <LoginForm />
+        <Menu isAuthenticated={this.props.isAuthenticated} />
+        <h2>Home</h2>
       </>
     );
   }
 }
 
-export default userIsNotAuthenticated(Home);
+export default userIsAuthenticated(Home);
