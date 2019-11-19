9a00e3bffee3508c534d2ff007ca2380d47355d6
Rob Spears <rob.sp3ars@gmail.com>
2019-11-19 12:26:23 -0500

    Add route to routing object for Login page

--- src/react/pages/index.js
+++ src/react/pages/index.js
@@ -1,9 +1,11 @@
 import Home from "./Home";
 import Profile from "./Profile";
 import NotFound from "./NotFound";
+import Login from "./Login";
 
 export default {
-  Home: { path: "/", component: Home },
+  Login: { path: "/", component: Login },
+  Home: { path: "/home", component: Home },
   Profile: { path: "/profile/:username", component: Profile },
   NotFound: { path: "*", component: NotFound }
 };
