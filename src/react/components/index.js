ecfc58880c734f8e4f4d4ed266d860fc44c431d0
Ty Schrock <ty.schrock14@gmail.com>
2019-11-21 10:00:45 -0500

    created basic user card for profile page

--- src/react/components/index.js
+++ src/react/components/index.js
@@ -1,7 +1,7 @@
 // components from own project
 export { default as LoginForm } from "./LoginForm";
 export { default as Menu } from "./Menu";
-
+export { default as UserCard } from "./UserCard"
 // components from other packages
 export { Switch, Route, Link } from "react-router-dom";
 export { default as Spinner } from "react-spinkit";
--- src/react/pages/Profile.js
+++ src/react/pages/Profile.js
@@ -1,16 +1,22 @@
 import React from "react";
 import { Menu } from "../components";
 import { userIsAuthenticated } from "../HOCs";
+import {UserCard} from "../components"
+// import "./Profile.css";
+
 
 class Profile extends React.Component {
   render() {
     return (
-      <>
-        <Menu isAuthenticated={this.props.isAuthenticated} />
-        <h2>Profile</h2>
-      </>
+      <React.Fragment>
+          <Menu isAuthenticated={this.props.isAuthenticated} />
+      
+          <UserCard/>
+         
+      </React.Fragment>
     );
   }
 }
 
 export default userIsAuthenticated(Profile);
+
