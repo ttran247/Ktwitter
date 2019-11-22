61ba8d7397ac26468d07c7c82151fb2af232ffbf
Rob Spears <rob.sp3ars@gmail.com>
2019-11-22 16:37:53 -0500

    Add route to Registration page

--- src/react/pages/index.js
+++ src/react/pages/index.js
@@ -2,10 +2,12 @@ import Home from "./Home";
 import Profile from "./Profile";
 import NotFound from "./NotFound";
 import Login from "./Login";
+import Registration from "./Registration";
 
 export default {
   Login: { path: "/", component: Login },
   Home: { path: "/home/:username", component: Home },
   Profile: { path: "/profile/:username", component: Profile },
+  Register: { path: "/register", component: Registration },
   NotFound: { path: "*", component: NotFound }
 };
