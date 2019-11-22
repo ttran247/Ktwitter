52c8399bfd91b9948a195779af3b29a48ea24256
Rob Spears <rob.sp3ars@gmail.com>
2019-11-22 09:41:53 -0500

    Add export of MessageList.js so it can be used elsewhere in application

--- src/react/components/index.js
+++ src/react/components/index.js
@@ -3,6 +3,7 @@ export { default as LoginForm } from "./LoginForm";
 export { default as Menu } from "./Menu";
 export { default as UserCard } from "./UserCard";
 export { default as NewMessageEntry } from "./NewMessageEntry";
+export { default as MessageList } from "./MessageList";
 // components from other packages
 export { Switch, Route, Link } from "react-router-dom";
 export { default as Spinner } from "react-spinkit";
