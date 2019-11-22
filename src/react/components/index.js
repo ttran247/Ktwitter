04e3b0e87736254773ba91a32193ff67c0d3d5e4
Ty Schrock <ty.schrock14@gmail.com>
2019-11-22 10:43:25 -0500

    deleted old user card

--- src/react/components/index.js
+++ src/react/components/index.js
@@ -1,7 +1,6 @@
 // components from own project
 export { default as LoginForm } from "./LoginForm";
 export { default as Menu } from "./Menu";
-export { default as UserCard } from "./UserCard";
 export { default as CardExampleCard } from "./UserCardNew"
 export { default as FeedExampleContentDate } from "./ProfileFeed"
 export { default as NewMessageEntry } from "./NewMessageEntry";
