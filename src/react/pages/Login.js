5d036e56deb7626f3b2a4c0f97b205d5e147aac4
Rob Spears <rob.sp3ars@gmail.com>
2019-11-22 20:51:57 -0500

    Render appropriate elements on Login landing page

--- src/react/pages/Login.js
+++ src/react/pages/Login.js
@@ -2,15 +2,35 @@ import React from "react";
 import { LoginForm, Menu } from "../components";
 import { userIsNotAuthenticated } from "../HOCs";
 import "./Login.css";
+import { Icon } from "semantic-ui-react";
 
 class Login extends React.Component {
   render() {
     return (
       <>
         <Menu />
-        <h2>Sign In</h2>
-        <div id="background">
-          <LoginForm />
+        <div id="loginContent">
+          <div id="loginHeader">
+            <h1>Kwitter</h1>
+            <h4>Log In Below</h4>
+          </div>
+          <div id="loginFormContainer">
+            <LoginForm />
+          </div>
+          <div id="loginGraphics">
+            <div id="graphic">
+              <Icon name="users" color="teal" size="big" /> <br />
+              Hear what people are talking about
+            </div>
+            <div id="graphic">
+              <Icon name="comments" color="teal" size="big" /> <br />
+              Join the conversation
+            </div>
+            <div id="graphic">
+              <Icon name="thumbs up" color="teal" size="big" /> <br />
+              Express your opinion
+            </div>
+          </div>
         </div>
       </>
     );
