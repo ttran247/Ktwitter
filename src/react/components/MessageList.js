185c6a196b30336360ae764ad197dbb42b2d7611
Rob Spears <rob.sp3ars@gmail.com>
2019-11-27 01:33:32 -0500

    Delete mock user data and load messages from API

--- src/react/components/MessageList.js
+++ src/react/components/MessageList.js
@@ -1,107 +1,67 @@
 import React from "react";
 import MessageCard from "./MessageCard";
-import {getMessageArray} from "../../redux/actionCreators"
-import {connect} from "react-redux"
-
-const messages = [
-  {
-    id: 980,
-    text: "This is my fifth Kwitter message.",
-    username: "jtjones22",
-    createdAt: "2019-11-20T20:35:06.752Z",
-    likes: []
-  },
-  {
-    id: 979,
-    text: "This is my fourth Kwitter message.",
-    username: "jtjones22",
-    createdAt: "2019-11-20T20:35:03.092Z",
-    likes: []
-  },
-  {
-    id: 978,
-    text: "This is my third Kwitter message.",
-    username: "jtjones22",
-    createdAt: "2019-11-20T20:34:58.952Z",
-    likes: []
-  },
-  {
-    id: 977,
-    text: "This is my second Kwitter message.",
-    username: "jtjones22",
-    createdAt: "2019-11-20T20:34:48.026Z",
-    likes: []
-  },
-  {
-    id: 976,
-    text: "This is my first Kwitter message.",
-    username: "jtjones22",
-    createdAt: "2019-11-20T20:34:30.285Z",
-    likes: []
-  },
-  {
-    id: 975,
-    text: "hello",
-    username: "test",
-    createdAt: "2019-11-20T20:22:10.258Z",
-    likes: []
-  },
-  {
-    id: 974,
-    text: "Hello world!",
-    username: "test",
-    createdAt: "2019-11-20T19:24:16.142Z",
-    likes: []
-  },
-  {
-    id: 973,
-    text: "Hello world!",
-    username: "test",
-    createdAt: "2019-11-20T19:24:13.896Z",
-    likes: []
-  },
-  {
-    id: 972,
-    text: "how's everyone doing?",
-    username: "test",
-    createdAt: "2019-11-20T02:54:29.182Z",
-    likes: []
-  },
-  {
-    id: 971,
-    text: "or maybe it's me you can't stand?",
-    username: "test",
-    createdAt: "2019-11-20T02:36:47.429Z",
-    likes: []
-  }
-];
+import { getMessageArray } from "../../redux/actionCreators";
+import { connect } from "react-redux";
+import "./MessageList.css";
 
 class MessageList extends React.Component {
-  
-  componentDidMount() {
-    this.props.getMessageArray()
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      messages: [],
+      loading: true
+    };
   }
-  
+
+  componentDidMount() {
+    this.props.getMessageArray();
+  }
+
+  componentDidUpdate(previousProps) {
+    if (this.props.messages && previousProps.messages !== this.props.messages) {
+      this.setState({ messages: this.props.messages, loading: false });
+    }
+  }
+
   render() {
-    return messages.map(message => {
-      return (
-        <MessageCard
-          username={message.username}
-          text={message.text}
-          createdAt={message.createdAt}
-          key={message.id}
-        />
-      );
-    });
+    const { messages } = this.state;
+    return (
+      <div id="listContainer">
+        {messages.map(message => {
+          const date = new Date(message.createdAt);
+          return (
+            <MessageCard
+              username={message.username}
+              text={message.text}
+              createdAt={`${date.toLocaleTimeString(navigator.language, {
+                month: "short",
+                day: "numeric",
+                year: "numeric",
+                hour: "numeric",
+                minute: "numeric"
+              })}`}
+              key={message.id}
+              id={message.id}
+            />
+          );
+        })}
+      </div>
+    );
   }
 }
 
 const mapDispatchToProps = dispatch => {
-    return {
-      getMessageArray: ()=> {
-        dispatch(getMessageArray()) 
-      }
+  return {
+    getMessageArray: () => {
+      dispatch(getMessageArray());
     }
-}
+  };
+};
+const mapStateToProps = state => {
+  return {
+    messages: state.messages.getMessageFeed.messages
+  };
+};
 
-export default connect(null, mapDispatchToProps)(MessageList)
+export default connect(mapStateToProps, mapDispatchToProps)(MessageList);
